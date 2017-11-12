001aa4c97d15bd752496247d86885df0b550ab7f
adry0094 <adripardoslopez@gmail.com>
2017-11-12 16:28:13 +0100

    Colocar botones y numero

---
+++ ES/Assets/Scripts/pantallaInici/numPlayersSelect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class numPlayersSelect : MonoBehaviour {
+
+	public void LoadByIndex(int index) {
+		//SceneManager.LoadScene (index);
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
