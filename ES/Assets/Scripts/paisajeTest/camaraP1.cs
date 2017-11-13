21ba330af9273bce02881d7818f3c7a696916bf9
cinthya1213 <cinthya.g12@hotmail.com>
2017-11-13 17:44:49 +0100

    camara sigue al jugador

---
+++ ES/Assets/Scripts/paisajeTest/camaraP1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camaraP1 : MonoBehaviour {
+
+	//objeto del cual se va hacer un seguimiento
+	public GameObject player;
+	private Vector3 distancia;
+
+	// Use this for initialization
+	void Start () {
+		//calculo la distancia entre la posicion del jugador y la posicion de la camara
+		distancia =	transform.position - player.transform.position;
+		distancia = new Vector3 (distancia.x, distancia.y + 2, distancia.z);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//cambiamos la posicion de la camara manteniendo siempre la distancia entre el jugador y la camara.
+		transform.position = player.transform.position + distancia;
+		
+	}
+}
