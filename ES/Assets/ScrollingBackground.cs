82ac2fd69e8f517242cbfc501f3b93edfd49cce2
mguell <guellmgj@gmail.com>
2017-11-22 19:30:31 +0100

    27- Fondo dinámico del menu principal

---
+++ ES/Assets/ScrollingBackground.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+	public float velocidad = 0.1f;
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector2 offset = new Vector2(Time.time * velocidad, 0);
+
+		GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}
