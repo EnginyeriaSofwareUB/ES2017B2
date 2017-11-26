5489ac3964197fa905c48adf0a6485c78bf7a667
llam05 <orla.marlon@gmail.com>
2017-11-26 02:04:51 +0100

    Cuando un jugador cae al agua o el suelo desaparece vuelve a la pantalla de selección de personajes. Una vez se implemente la pantalla de final de partida se ha de redirigir a esta.

--- ES/Assets/Scripts/paisajeTest/MapLimitController.cs
+++ ES/Assets/Scripts/paisajeTest/MapLimitController.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MapLimitController : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.tag.Contains("Jugador") || col.tag == "bullet") {
 			Destroy (col.gameObject);
+			SceneManager.LoadScene (2);
 		}
 	}
 }
