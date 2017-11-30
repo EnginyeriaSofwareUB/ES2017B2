5ffc36e67ab9155940d8f40c0e8eba946685ab89
llam05 <orla.marlon@gmail.com>
2017-12-01 00:31:27 +0100

    Solución bug en selección de armas. Cambiaba el personaje.

---
+++ ES/Assets/Scripts/select2Players/prevArmaP1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prevArmaP1 : MonoBehaviour {
+
+	public GameObject personaje1;
+	public GameObject personaje2;
+	public GameObject personaje3;
+	public GameObject personaje4;
+	public GameObject personaje5;
+
+	public void OnMouseDown() {
+		if (Input.GetMouseButtonDown (0)) {
+			if (personaje1.activeSelf) {
+				personaje1.SetActive (false);
+				personaje2.SetActive (false);
+				personaje3.SetActive (false);
+				personaje4.SetActive (false);
+				personaje5.SetActive (true);
+				//ProjectVars.Instance.playersPrefabs [transform.tag] = "Robot";
+			} else if (personaje2.activeSelf) {
+				personaje1.SetActive (true);
+				personaje2.SetActive (false);
+				personaje3.SetActive (false);
+				personaje4.SetActive (false);
+				personaje5.SetActive (false);
+				//ProjectVars.Instance.playersPrefabs[transform.tag] = "Woman_warrior_1";
+			} else if (personaje3.activeSelf) {
+				personaje1.SetActive (false);
+				personaje2.SetActive (true);
+				personaje3.SetActive (false);
+				personaje4.SetActive (false);
+				personaje5.SetActive (false);
+				//ProjectVars.Instance.playersPrefabs[transform.tag] = "Woman_warrior_2";
+			} else if (personaje4.activeSelf) {
+				personaje1.SetActive (false);
+				personaje2.SetActive (false);
+				personaje3.SetActive (true);
+				personaje4.SetActive (false);
+				personaje5.SetActive (false);
+				//ProjectVars.Instance.playersPrefabs[transform.tag] = "Knight";
+			}  else if (personaje5.activeSelf) {
+				personaje1.SetActive (false);
+				personaje2.SetActive (false);
+				personaje3.SetActive (false);
+				personaje4.SetActive (true);
+				personaje5.SetActive (false);
+				//ProjectVars.Instance.playersPrefabs[transform.tag] = "Knight";
+			}
+		}
+	}
+}
