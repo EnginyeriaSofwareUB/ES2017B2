ed7c749f053a1d73e5d7a9c8221c95543cc43e77
adry0094 <adripardoslopez@gmail.com>
2017-12-02 14:17:49 +0100

    Power up de recuperar energia

--- ES/Assets/Scripts/paisajeTest/powerup_collision.cs
+++ ES/Assets/Scripts/paisajeTest/powerup_collision.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class powerup_collision : MonoBehaviour {
 
-	void OnTriggerEnter(){
-		
+  private GameObject energyBar; // variable apra gestionar la barra de energia
+
+	// Use this for initialization
+	void Start () {
+  	energyBar = GameObject.Find("EnergyBar");
 	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		//Comprobamos que tipo de power up es
+		if(gameObject.tag == "powerup_energy"){
+			energyBar.SendMessage ("setEnergy", 100);//Recuperamos energia
+		}
+		else if(gameObject.tag == "powerup_health"){
+			
+		}
+		Destroy(gameObject);
+	}
+
 
 }
