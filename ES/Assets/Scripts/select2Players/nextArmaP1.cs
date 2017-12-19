54740696b8c7269561496ab6f6818faa0c9859b4
llam05 <orla.marlon@gmail.com>
2017-12-19 02:17:53 +0100

    Colocado power stat a cada arma en los menús de selección de los personajes

--- ES/Assets/Scripts/select2Players/nextArmaP1.cs
+++ ES/Assets/Scripts/select2Players/nextArmaP1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class nextArmaP1 : MonoBehaviour {
 
@@ -10,6 +11,7 @@ public class nextArmaP1 : MonoBehaviour {
 	public GameObject personaje3;
 	public GameObject personaje4;
 	public GameObject personaje5;
+	public Text texto = null; 
 
 	public void OnMouseDown() {
 
@@ -22,6 +24,7 @@ public class nextArmaP1 : MonoBehaviour {
 				personaje5.SetActive (false);
 				ProjectVars.Instance.weaponsPlayers [transform.tag] = "pistola";
 				ProjectVars.Instance.daño [transform.tag] = 30;
+				texto.text = "30";
 			} else if (personaje2.activeSelf) {
 				personaje1.SetActive (false);
 				personaje2.SetActive (false);
@@ -30,6 +33,7 @@ public class nextArmaP1 : MonoBehaviour {
 				personaje5.SetActive (false);
 				ProjectVars.Instance.weaponsPlayers[transform.tag] = "arco";
 				ProjectVars.Instance.daño [transform.tag] = 10;
+				texto.text = "10";
 			} else if (personaje3.activeSelf) {
 				personaje1.SetActive (false);
 				personaje2.SetActive (false);
@@ -38,6 +42,7 @@ public class nextArmaP1 : MonoBehaviour {
 				personaje5.SetActive (false);
 				ProjectVars.Instance.weaponsPlayers[transform.tag] = "arma_5";
 				ProjectVars.Instance.daño [transform.tag] = 80;
+				texto.text = "80";
 			} else if (personaje4.activeSelf) {
 				personaje1.SetActive (false);
 				personaje2.SetActive (false);
@@ -46,6 +51,7 @@ public class nextArmaP1 : MonoBehaviour {
 				personaje5.SetActive (true);
 				ProjectVars.Instance.weaponsPlayers[transform.tag] = "pistola_2";
 				ProjectVars.Instance.daño [transform.tag] = 40;
+				texto.text = "40";
 			}  else if (personaje5.activeSelf) {
 				personaje1.SetActive (true);
 				personaje2.SetActive (false);
@@ -54,6 +60,7 @@ public class nextArmaP1 : MonoBehaviour {
 				personaje5.SetActive (false);
 				ProjectVars.Instance.weaponsPlayers[transform.tag] = "arma_4";
 				ProjectVars.Instance.daño [transform.tag] = 50;
+				texto.text = "50";
 			}  
 		}
 	}
--- ES/Assets/Scripts/select2Players/prevArmaP1.cs
+++ ES/Assets/Scripts/select2Players/prevArmaP1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class prevArmaP1 : MonoBehaviour {
 
@@ -9,6 +10,7 @@ public class prevArmaP1 : MonoBehaviour {
 	public GameObject personaje3;
 	public GameObject personaje4;
 	public GameObject personaje5;
+	public Text texto;
 
 	public void OnMouseDown() {
 		if (Input.GetMouseButtonDown (0)) {
@@ -20,6 +22,7 @@ public class prevArmaP1 : MonoBehaviour {
 				personaje5.SetActive (true);
 				ProjectVars.Instance.weaponsPlayers [transform.tag] = "pistola_2";
 				ProjectVars.Instance.daño [transform.tag] = 40;
+				texto.text = "40";
 			} else if (personaje2.activeSelf) {
 				personaje1.SetActive (true);
 				personaje2.SetActive (false);
@@ -28,6 +31,7 @@ public class prevArmaP1 : MonoBehaviour {
 				personaje5.SetActive (false);
 				ProjectVars.Instance.weaponsPlayers[transform.tag] = "arma_4";
 				ProjectVars.Instance.daño [transform.tag] = 50;
+				texto.text = "50";
 			} else if (personaje3.activeSelf) {
 				personaje1.SetActive (false);
 				personaje2.SetActive (true);
@@ -36,6 +40,7 @@ public class prevArmaP1 : MonoBehaviour {
 				personaje5.SetActive (false);
 				ProjectVars.Instance.weaponsPlayers[transform.tag] = "pistola";
 				ProjectVars.Instance.daño [transform.tag] = 30;
+				texto.text = "30";
 			} else if (personaje4.activeSelf) {
 				personaje1.SetActive (false);
 				personaje2.SetActive (false);
@@ -44,6 +49,7 @@ public class prevArmaP1 : MonoBehaviour {
 				personaje5.SetActive (false);
 				ProjectVars.Instance.weaponsPlayers[transform.tag] = "arco";
 				ProjectVars.Instance.daño [transform.tag] = 10;
+				texto.text = "10";
 			}  else if (personaje5.activeSelf) {
 				personaje1.SetActive (false);
 				personaje2.SetActive (false);
@@ -52,6 +58,7 @@ public class prevArmaP1 : MonoBehaviour {
 				personaje5.SetActive (false);
 				ProjectVars.Instance.weaponsPlayers[transform.tag] = "arma_5";
 				ProjectVars.Instance.daño [transform.tag] = 80;
+				texto.text = "80";
 			}
 		}
 	}
